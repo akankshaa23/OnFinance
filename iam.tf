3dade4911cf5b2f5b84087cd4209bcff4c9a95ce
akankshaa23 <82812105+akankshaa23@users.noreply.github.com>
2025-04-05 17:57:30 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -89,3 +89,14 @@ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   role       = aws_iam_role.worker-node.name
 }
 
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name
+}
+
