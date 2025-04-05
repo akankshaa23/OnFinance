b2b8e322246c8ccdd8fb73210c28129c20c809ff
akankshaa23 <82812105+akankshaa23@users.noreply.github.com>
2025-04-05 17:49:46 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,19 +1,40 @@
 resource "aws_eks_cluster" "eks" {
   name     = "OF-eks-cluster"
   role_arn = aws_iam_role.eks.arn
+
   vpc_config {
-    subnet_ids = concat(aws_subnet.OF-subnets-public[*].id, aws_subnet.OF-subnets-private[*].id)
+    subnet_ids = [
+      aws_subnet.OF-subnet-1.id,
+      aws_subnet.OF-subnet-2.id
+    ]
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy
+  ]
 }
 
 resource "aws_eks_node_group" "node_group" {
-  cluster_name  = aws_eks_cluster.eks.name
-  node_role_arn = aws_iam_role.worker-node.arn
-  subnet_ids    = aws_subnet.OF-subnets-private[*].id
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "OF-node-group"
+  node_role_arn   = aws_iam_role.worker-node.arn
+  subnet_ids      = [
+    aws_subnet.OF-subnet-1.id,
+    aws_subnet.OF-subnet-2.id
+  ]
+
   scaling_config {
     desired_size = 2
-    min_size     = 1
     max_size     = 3
+    min_size     = 1
   }
+
+depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
 }
 
