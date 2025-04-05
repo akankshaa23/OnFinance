8f74f2054297be3272c370a1ebdade2c6cebafbc
akankshaa23 <82812105+akankshaa23@users.noreply.github.com>
2025-04-05 19:05:41 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -4,19 +4,19 @@ resource "aws_iam_role" "eks" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Effect = "Allow",
         Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
+          Service = "eks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
     ]
   })
 }
+
 
 
 # To create iam role for worker nodes
