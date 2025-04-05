c739bd417bf35ad5fd02258a75b553633a31cb89
akankshaa23 <82812105+akankshaa23@users.noreply.github.com>
2025-04-05 17:41:18 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,18 @@
-# To Create VPC
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "OF-vpc" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "OF-vpc"
   }
 }
-
 
 # Subnet 1 - AZ 1
 resource "aws_subnet" "OF-subnet-1" {
@@ -32,21 +38,6 @@ resource "aws_subnet" "OF-subnet-2" {
   }
 }
 
-
-# To Create Private Subnets 
-
-resource "aws_subnet" "OF-subnets-private" {
-  vpc_id     = aws_vpc.OF-vpc.id
-  cidr_block = "10.10.3.0/24"
-
-  tags = {
-    Name = "OF-subnets-private"
-  }
-}
-
-
-# To Create Internet Gateway
-
 resource "aws_internet_gateway" "OF-igw" {
   vpc_id = aws_vpc.OF-vpc.id
 
@@ -54,9 +45,6 @@ resource "aws_internet_gateway" "OF-igw" {
     Name = "OF-igw"
   }
 }
-
-
-# To Create Route Tables
 
 resource "aws_route_table" "OF-rt" {
   vpc_id = aws_vpc.OF-vpc.id
@@ -71,40 +59,12 @@ resource "aws_route_table" "OF-rt" {
   }
 }
 
-
-# Association of public subnet to route table 
-
-resource "aws_route_table_association" "OF-rta-public" {
+resource "aws_route_table_association" "OF-rt-assoc-1" {
   subnet_id      = aws_subnet.OF-subnet-1.id
   route_table_id = aws_route_table.OF-rt.id
 }
 
-
-# Association of private subnet to route table 
-
-resource "aws_route_table_association" "OF-rta-private" {
-  subnet_id      = aws_subnet.OF-subnets-private.id
+resource "aws_route_table_association" "OF-rt-assoc-2" {
+  subnet_id      = aws_subnet.OF-subnet-2.id
   route_table_id = aws_route_table.OF-rt.id
-}
-
-
-# To Create Security Group
-
-resource "aws_security_group" "OF-sg" {
-  name   = "OF-sg"
-  vpc_id = aws_vpc.OF-vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "OF-sgi" {
-  ip_protocol       = "tcp"
-  security_group_id = aws_security_group.OF-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "OF-sge" {
-  ip_protocol       = "-1"
-  security_group_id = aws_security_group.OF-sg.id
-  cidr_ipv4         = "0.0.0.0/0"
 }
